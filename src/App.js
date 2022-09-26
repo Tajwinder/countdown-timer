4e5666f62fbb669d0267a69b4ed645db7b04e351
Tajwinder <55351458+Tajwinder@users.noreply.github.com>
2022-09-26 23:09:26 +0530

    TS | create react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CountdownTimer from './components/ContdownTimer/CountdownTimer'
+
+function App() {
+  return (
+    <div className="App">
+<CountdownTimer/>
+    </div>
+  );
+}
+
+export default App;
