7162dba13c40a8da768a519f2f535537fd4832e9
Tajwinder <55351458+Tajwinder@users.noreply.github.com>
2022-09-26 23:40:50 +0530

    TS | stop the timer as contdown reaches zero

--- src/components/ContdownTimer/CountdownTimer.jsx
+++ src/components/ContdownTimer/CountdownTimer.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 const CountdownTimer = () => {
-  const [time, setTime] = useState(10);
+  const [time, setTime] = useState(10),
+    [stop, setStop] = useState(false);
 
-  useEffect(() =>{
-      console.log('useEffect ran')
-setInterval(()=>setTime(time=>time-1),1000)
-  },[])
+  useEffect(() => {
+    console.log("useEffect running");
+    let interval;
+    if (!stop) {
+      interval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [stop]);
+
+  useEffect(() => {
+    if (!time) {
+      setStop(true);
+    }
+  }, [time]);
 
   return (
     <div>
